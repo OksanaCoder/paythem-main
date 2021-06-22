65633bfbea7c2a271b28a4f7ed8db045323e912a
xena <oksishukh@yahoo.com>
2021-06-22 15:20:28 +0300

    first commit

--- src/js/config/api.js
+++ src/js/config/api.js
@@ -1,6 +1,5 @@
 const API = {
-  baseUrl: 'http://localhost:5000/api',
-  // baseUrl: 'http://157.230.112.210:5000/api',
+  baseUrl: 'http://157.230.112.210:5000/api',
   redirect_uri: window.location.origin,
 };
 
